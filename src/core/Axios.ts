8284c14a37bbd850a2bfe6f03ef24674524fdcd8
minzhang <569835014@qq.com>
2019-06-04 07:59:33 +0800

    feat(croe): 开发axios类

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -2,7 +2,15 @@ import { AxiosPromise, AxiosRequestConfig, Method } from '../types'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  requeset(config: AxiosRequestConfig): AxiosPromise {
+  request(url?: any, config?: any): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
     return dispatchRequest(config)
   }
 
@@ -25,15 +33,17 @@ export default class Axios {
   post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('delete', url, data, config)
   }
+
   put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('put', url, data, config)
   }
+
   patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise {
     return this._requestMethodWithData('patch', url, data, config)
   }
 
   _requestMethodWithData(method: Method, url: string, data?: any, config?: AxiosRequestConfig) {
-    return this.requeset(
+    return this.request(
       Object.assign(config || {}, {
         method: method,
         url,
@@ -43,7 +53,7 @@ export default class Axios {
   }
 
   _requestMethodWithoutData(method: Method, url: string, config?: AxiosRequestConfig) {
-    return this.requeset(
+    return this.request(
       Object.assign(config || {}, {
         method: method,
         url
